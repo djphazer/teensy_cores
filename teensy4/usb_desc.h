a3129330c22b8ef787bff8d47d79fa7ca8c6f154
Nicholas J. Michalek <djphazer@gmail.com>
2025-11-18 05:42:55 -0500

    new config for USB_AUDIO + USB_MIDI

--- teensy4/usb_desc.h
+++ teensy4/usb_desc.h
@@ -441,6 +441,48 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define ENDPOINT4_CONFIG	ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
   #define ENDPOINT5_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_INTERRUPT
 
+#elif defined(USB_AUDIO) && defined(USB_MIDI)
+  #define VENDOR_ID		0x16C0
+  #define PRODUCT_ID		0x04D2
+  #define MANUFACTURER_NAME	{'T','e','e','n','s','y','d','u','i','n','o'}
+  #define MANUFACTURER_NAME_LEN	11
+  #define PRODUCT_NAME		{'T','e','e','n','s','y',' ','A','u','d','i','o'}
+  #define PRODUCT_NAME_LEN	12
+  #define EP0_SIZE		64
+  #define NUM_ENDPOINTS         5
+  #define NUM_INTERFACE		5
+  #define SEREMU_INTERFACE      0	// Serial emulation
+  #define SEREMU_TX_ENDPOINT    2
+  #define SEREMU_TX_SIZE        64
+  #define SEREMU_TX_INTERVAL    1
+  #define SEREMU_RX_ENDPOINT    2
+  #define SEREMU_RX_SIZE        32
+  #define SEREMU_RX_INTERVAL    2
+  #define AUDIO_INTERFACE	1	// Audio (uses 3 consecutive interfaces)
+  #define AUDIO_TX_ENDPOINT     3
+  #define AUDIO_CHANNELS        2 // Must be a multiple of 2
+  #define AUDIO_FREQUENCY       AUDIO_SAMPLE_RATE_EXACT
+  #define AUDIO_SAMPLE_BYTES    (sizeof ((audio_block_t*) 0)->data[0])
+  #define AUDIO_BIT_DEPTH       (AUDIO_SAMPLE_BYTES * 8)
+  #define AUDIO_TX_SIZE         ((int)(AUDIO_FREQUENCY / 1000U) + 1) * AUDIO_CHANNELS * AUDIO_SAMPLE_BYTES
+  #define AUDIO_RX_ENDPOINT     3
+  #define AUDIO_RX_SIZE         AUDIO_TX_SIZE
+  #define AUDIO_SYNC_ENDPOINT	4
+  #define AUDIO_POLLING_INTERVAL 1 // The bInterval polling value in microframes, 4 => 2^4 => 8 microframes == 1ms
+  #define ENDPOINT2_CONFIG	ENDPOINT_RECEIVE_INTERRUPT + ENDPOINT_TRANSMIT_INTERRUPT
+  #define ENDPOINT3_CONFIG	ENDPOINT_RECEIVE_ISOCHRONOUS + ENDPOINT_TRANSMIT_ISOCHRONOUS
+  #define ENDPOINT4_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_ISOCHRONOUS
+
+  #define MIDI_INTERFACE        4	// MIDI
+  #define MIDI_NUM_CABLES       1
+  #define MIDI_TX_ENDPOINT      5
+  #define MIDI_TX_SIZE_12       64
+  #define MIDI_TX_SIZE_480      512
+  #define MIDI_RX_ENDPOINT      5
+  #define MIDI_RX_SIZE_12       64
+  #define MIDI_RX_SIZE_480      512
+  #define ENDPOINT5_CONFIG	ENDPOINT_RECEIVE_BULK + ENDPOINT_TRANSMIT_BULK
+
 #elif defined(USB_MIDI)
   #define VENDOR_ID		0x16C0
   #define PRODUCT_ID		0x0485
