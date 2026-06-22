fdbc3b7e487345a9d4edbb1fba8c53ca7bcdd84b
Nicholas J. Michalek <djphazer@gmail.com>
2026-06-22 01:42:39 -0400

    reorder USB MIDI + AUDIO config; adjust SEREMU size/intervals
    Should help USB Audio stability. Ideally, we should just use stock
    USB_MIDI_AUDIO_SERIAL config, but Paul's Screen Capture for O_C relies
    on Seremu. If it can be made to use regular CDC Serial instead, this
    problem can go away.

--- teensy4/usb_desc.h
+++ teensy4/usb_desc.h
@@ -451,14 +451,15 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define EP0_SIZE		64
   #define NUM_ENDPOINTS         5
   #define NUM_INTERFACE		5
-  #define SEREMU_INTERFACE      0	// Serial emulation
+
+  #define SEREMU_INTERFACE      4	// Serial emulation
   #define SEREMU_TX_ENDPOINT    2
   #define SEREMU_TX_SIZE        64
-  #define SEREMU_TX_INTERVAL    1
+  #define SEREMU_TX_INTERVAL    4
   #define SEREMU_RX_ENDPOINT    2
-  #define SEREMU_RX_SIZE        32
-  #define SEREMU_RX_INTERVAL    2
-  #define AUDIO_INTERFACE	1	// Audio (uses 3 consecutive interfaces)
+  #define SEREMU_RX_SIZE        64
+  #define SEREMU_RX_INTERVAL    4
+  #define AUDIO_INTERFACE	0	// Audio (uses 3 consecutive interfaces)
   #define AUDIO_TX_ENDPOINT     3
   #define AUDIO_CHANNELS        2 // Must be a multiple of 2
   #define AUDIO_FREQUENCY       AUDIO_SAMPLE_RATE_EXACT
@@ -473,7 +474,7 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define ENDPOINT3_CONFIG	ENDPOINT_RECEIVE_ISOCHRONOUS + ENDPOINT_TRANSMIT_ISOCHRONOUS
   #define ENDPOINT4_CONFIG	ENDPOINT_RECEIVE_UNUSED + ENDPOINT_TRANSMIT_ISOCHRONOUS
 
-  #define MIDI_INTERFACE        4	// MIDI
+  #define MIDI_INTERFACE        3	// MIDI
   #define MIDI_NUM_CABLES       1
   #define MIDI_TX_ENDPOINT      5
   #define MIDI_TX_SIZE_12       64
